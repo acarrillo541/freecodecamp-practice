ef0bb27b732d96a5a0112253a58f578eb7ebd6ad
Alan <acarrillo4@mail.csuchico.edu>
2023-02-17 19:23:14 -0800

    use the .env file: working on free codecamp

--- myApp.js
+++ myApp.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+let express = require('express');
+let app = express();
+
+
+absolutePath=__dirname + "/views/index.html";
+path=__dirname + "/public";
+
+app.use("/public",express.static(path));
+
+app.get("/", function(req,res){
+  res.sendFile(absolutePath);
+});
+
+app.get("/json", function(req,res){
+  res.json({"message":"Hello json"});
+});
+
+console.log("Hello Express");
+
+
+module.exports = app;
